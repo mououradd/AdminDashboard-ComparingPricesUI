8cecad36d19f747f1c65d18bd571e688b7189ff0
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-06-19 19:01:41 +0300

    added Category & SubCat & Brand to AddProduct Stage

--- src/app/components/ProductDashboard/add-product/add-product.component.ts
+++ src/app/components/ProductDashboard/add-product/add-product.component.ts
@@ -1,3 +1,5 @@
+import { Category, SubCategory } from './../../../models/category';
+import { SelectItem } from 'primeng/api';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -8,61 +10,125 @@ import { StepsMenuComponent } from '../../steps-menu/steps-menu.component';
 import { ScrapingServiceService } from '../../../services/scraping-service.service';
 import { InputTextModule } from 'primeng/inputtext';
 import { Router } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+// Toaster
+import { MessageService } from 'primeng/api';
+
 @Component({
-  selector: 'app-add-product',
-  standalone: true,
-  templateUrl: './add-product.component.html',
-  imports: [
-    CommonModule,
-    FormsModule,
-    ButtonModule,
-    DialogModule,
-    InputTextModule,
-    StepsMenuComponent,
-  ],
+    selector: 'app-add-product',
+    standalone: true,
+    templateUrl: './add-product.component.html',
+    imports: [
+        CommonModule,
+        FormsModule,
+        ButtonModule,
+        DialogModule,
+        InputTextModule,
+        StepsMenuComponent,
+        DropdownModule,
+        ProgressSpinnerModule,
+    ],
+    providers: [MessageService]
 })
 export class AddProductComponent {
-  constructor(
-    public scrapingService: ScrapingServiceService,
-    //Router to Navigate
-    public router: Router
-  ) {}
-  submitted: boolean = false;
-  ngOnInit() {}
+    //Variables
+    Category: any;
+    SubCategory: any;
+    Brand: any;
+    //Data
+    Categories:any;
+    //Control
+    submitted: boolean = false;
+    isLoading: boolean = true;
+    isScraping: boolean = false;
+    // Constructor
+    constructor(
+        public scrapingService: ScrapingServiceService,
+        public router: Router,
+        private messageService: MessageService
+    ) {}
+    //Init
+    ngOnInit() {
+        this.isLoading = false;
+        this.scrapingService.GetCategories().subscribe(
+            // Success
+            (data) => {
+                this.Categories=data;
+                this.Category = this.Categories[0];
+                console.log(data);
+            },
+            // Error
+            (error) => {
+                console.error('Error', error);
+            }
+        );
+    }
+    //Functions
+    onCategoryChange() {
+        this.scrapingService.scrapingData.productPostDTO.subCategoryId =this.Category.id;
+    }
+    onSubCategoryChange() {
+        this.scrapingService.scrapingData.productPostDTO.subCategoryId =this.SubCategory.id;
+    }
+    onBrandChange() {
+        this.scrapingService.scrapingData.productPostDTO.brandId =this.Brand.id;
+    }
 
-  addUrl(): void {
-    this.scrapingService.addUrl();
-  }
+    addUrl(): void {
+        this.scrapingService.addUrl();
+    }
 
-  deleteUrl(index: number): void {
-    this.scrapingService.deleteUrl(index);
-  }
+    deleteUrl(index: number): void {
+        this.scrapingService.deleteUrl(index);
+    }
 
-  saveProduct() {}
+    saveProduct() {}
+    // Navigation
+    next() {
+        this.isScraping = false;
+        console.log(this.scrapingService.urls);
+        console.log(this.scrapingService.scrapingData);
+        this.scrapingService.GetData(this.scrapingService.urls).subscribe(
+            // Success
+            (data) => {
+                console.log(data);
+                if (data.length > 0) {
+                    this.scrapingService.scrapingData.productDetailDTO = data;
+                    this.isScraping = true;
+                    this.router.navigate([
+                        '/admin/products/add-product/review',
+                    ]);
+                } else {
+                    // Toaster
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail: 'No data found',
+                    });
+                    //Display Error
+                    console.error('No data found');
+                }
+            },
+            // Error
+            (error) => {
+                console.error('Error', error);
+            }
+        );
+    }
 
-  next() {
-    // console.log(this.scrapingService.scrapingData);
-    // this.scrapingService
-    //   .GetImages([this.scrapingService.urls[0]])
-    //   .subscribe((data) => {
-    //     this.scrapingService.scrapingData.productImageDTO.images =
-    //       data[0].images;
-    //   });
-    this.scrapingService
-      .GetImages(this.scrapingService.urls)
-      .subscribe((data) => {
-        this.scrapingService.scrapingData.productDetailDTO = data;
-        // this.scrapingService.scrapingData.productImageDTO.images = data[0].images;
-        this.router.navigate(['/admin/products/add-product/review']);
-      });
+    back() {
+        this.router.navigate(['/admin/products']);
+    }
 
-  }
-
-  back() {
-    this.router.navigate(['/admin/products']);
-  }
-
-  isNextButtonEnabled(): boolean {
-    return !this.scrapingService.urls.some(url => url === 'https://www.example.com/') && this.scrapingService.urls.length > 0;
-  }
+    isNextButtonEnabled(): boolean {
+        return (
+            !this.scrapingService.urls.some(
+                (url) => url === 'https://www.example.com/'
+            ) && this.scrapingService.urls.length > 0 && this.Brand != null
+            && this.Category != null && this.SubCategory != null
+             && this.scrapingService.scrapingData.productPostDTO.name_Global != ''
+             && this.scrapingService.scrapingData.productPostDTO.description_Global != ''
+        );
+    }
 }
--- src/app/services/scraping-service.service.ts
+++ src/app/services/scraping-service.service.ts
@@ -1,86 +1,87 @@
 import { Injectable } from '@angular/core';
 import {
-  ScrapingData,
-  ProductDetailDTO,
-  ProductImageDTO,
-  ProductPostDTO,
+    ScrapingData,
+    ProductDetailDTO,
+    ProductImageDTO,
+    ProductPostDTO,
 } from './scraping-service.types';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ScrapingServiceService {
-  constructor(
-    //Inject HttpClient
-    private http: HttpClient
-  ) {}
-  //Scraping URL
-  private scrapingUrl = 'http://localhost:5066/api/AddProduct/AddProduct';
-  private SrapeImageUrl = 'https://price-comparison-scraper.onrender.com/scrape/';
-  public GetImages(url: string[]): Observable<any> {
-    return this.http.post(this.SrapeImageUrl, { urls: url });
-  }
-  //Scrape Data
-  public SaveData(data: ScrapingData): Observable<any> {
-    return this.http.post(this.scrapingUrl, data);
-  }
-  // Print Data
-  public PrintData() {
-    console.log(this.scrapingData);
-  }
-  //#region Urls
-  public urls: string[] = ['https://www.example.com/'];
-  addUrl(): void {
-    this.urls.push('');
-  }
-  deleteUrl(index: number): void {
-    if (this.urls.length === 1) {
-      return;
+    constructor(
+        //Inject HttpClient
+        private http: HttpClient
+    ) {}
+    //Scraping URL
+    private ApiUrl = 'http://localhost:2030/';
+  
+    private ScrapeUrl ='https://price-comparison-scraper.onrender.com/scrape/';
+    public GetData(url: string[]): Observable<any> {
+        return this.http.post(this.ScrapeUrl, { urls: url });
     }
-    this.urls.splice(index, 1);
-  }
-  //End region
-  //Product Post DTO
-  public productPostDTO: ProductPostDTO = {
-    name_Local: '',
-    name_Global: '',
-    description_Local: '',
-    description_Global: '',
-    subCategoryId: 1,
-    brandId: 1,
-  };
-  //Product Detail DTO
-  public productDetailDTO: ProductDetailDTO = {
-    prodId: 1,
-    domainId: 1,
-    productLink1: '',
-    status: '',
-    lastUpdated: new Date(),
-    lastScraped: new Date(),
-    id: 1,
-    name_Local: '',
-    name_Global: '',
-    description_Local: '',
-    description_Global: '',
-    price: 1,
-    rating: 1,
-    isAvailable: true,
-    brand: '',
-    images: ['assets/demo/images/galleria/galleria1.jpg'],
-  };
-  //Product Image DTO
-  public productImageDTO: ProductImageDTO = {
-    images: ['assets/demo/images/galleria/galleria1.jpg'],
-  };
-  //Scraping Data
-  public scrapingData: ScrapingData = {
-    productPostDTO: this.productPostDTO,
-    productDetailDTO: [this.productDetailDTO],
-    // productImageDTO: this.productImageDTO,
-  };
-  // Function to check if there are any URLs added
-  // public isUrlsAdded(): boolean {
-  //   return this.urls.length > 0;
-  // }
+    //Scrape Data
+    public SaveData(data: ScrapingData): Observable<any> {
+        return this.http.post(this.ApiUrl + 'api/AddProduct/AddProduct', data);
+    }
+    // Get Categories
+    public GetCategories(): Observable<any> {
+        return this.http.get(this.ApiUrl + 'api/Category');
+    }
+    // Print Data
+    public PrintData() {
+        console.log(this.scrapingData);
+    }
+    //#region Urls
+    public urls: string[] = ['https://www.example.com/'];
+
+    addUrl(): void {
+        this.urls.push('');
+    }
+    deleteUrl(index: number): void {
+        if (this.urls.length === 1) {
+            return;
+        }
+        this.urls.splice(index, 1);
+    }
+    //End region
+    //Product Post DTO
+    public productPostDTO: ProductPostDTO = {
+        name_Local: '',
+        name_Global: '',
+        description_Local: '',
+        description_Global: '',
+        subCategoryId: 1,
+        brandId: 1,
+    };
+    //Product Detail DTO
+    public productDetailDTO: ProductDetailDTO = {
+        prodId: 1,
+        domainId: 1,
+        productLink1: '',
+        status: '',
+        lastUpdated: new Date(),
+        lastScraped: new Date(),
+        id: 1,
+        name_Local: '',
+        name_Global: '',
+        description_Local: '',
+        description_Global: '',
+        price: 1,
+        rating: 1,
+        isAvailable: true,
+        brand: '',
+        images: ['assets/demo/images/galleria/galleria1.jpg'],
+    };
+    //Product Image DTO
+    public productImageDTO: ProductImageDTO = {
+        images: ['assets/demo/images/galleria/galleria1.jpg'],
+    };
+    //Scraping Data
+    public scrapingData: ScrapingData = {
+        productPostDTO: this.productPostDTO,
+        productDetailDTO: [this.productDetailDTO],
+    };
 }
