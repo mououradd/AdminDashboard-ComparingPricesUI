70e2049442c19666b307c4149ac5779793de5276
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-06-24 20:36:01 +0300

    updated validation with formGroup for better Performance

--- src/app/components/ProductDashboard/add-product/add-product.component.ts
+++ src/app/components/ProductDashboard/add-product/add-product.component.ts
@@ -2,7 +2,15 @@ import { Category, SubCategory } from './../../../models/category';
 import { SelectItem, Message } from 'primeng/api';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import {
+    AbstractControl,
+    FormArray,
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    Validators,
+} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../models/product';
@@ -22,7 +30,7 @@ import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from 'primeng/message';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputGroupModule } from 'primeng/inputgroup';
-
+import { validUrlValidator } from './validUrl';
 @Component({
     selector: 'app-add-product',
     standalone: true,
@@ -45,42 +53,56 @@ import { InputGroupModule } from 'primeng/inputgroup';
         InputGroupAddonModule,
         InputGroupModule,
         DropdownModule,
+        ReactiveFormsModule,
     ],
     providers: [MessageService],
 })
 export class AddProductComponent {
-    //Variables
+    productForm: FormGroup;
     Category: any;
     SubCategory: any;
     Brand: any;
-    //Data
     Categories: any;
-    //Control
-    submitted: boolean = false;
     isLoading: boolean = true;
     isScraping: boolean = false;
-    // Constructor
-    form: FormGroup = this.formbuilder.group({
-        Local_name: [null],
-    });
+    Furls: AbstractControl<any>[];
 
     constructor(
+        private fb: FormBuilder,
         public scrapingService: ScrapingServiceService,
         public router: Router,
-        private messageService: MessageService,
-        private formbuilder: FormBuilder
+        private messageService: MessageService
     ) {}
-    //Init
+
     ngOnInit() {
+        this.productForm = this.fb.group({
+            name_Global: ['', Validators.required],
+            description_Global: [
+                '',
+                [Validators.required, Validators.minLength(10)],
+            ],
+            name_Local: ['', Validators.required],
+            description_Local: [
+                '',
+                [Validators.required, Validators.minLength(10)],
+            ],
+            Category: [null, Validators.required],
+            SubCategory: [null, Validators.required],
+            Brand: [null, Validators.required],
+            urls: this.fb.array([
+                this.fb.group({
+                    url: ['', [Validators.required, validUrlValidator()]],
+                }),
+            ]),
+        });
+        this.Furls = (this.productForm.get('urls') as FormArray).controls;
+
         this.scrapingService.GetCategories().subscribe(
-            // Success
             (data) => {
                 this.isLoading = false;
                 this.Categories = data;
                 this.Category = this.Categories[0];
-                console.log(data);
             },
-            // Error
             (error) => {
                 this.show(
                     'error',
@@ -89,90 +111,130 @@ export class AddProductComponent {
             }
         );
     }
-    //Functions
+
+    get urls(): FormArray {
+        console.log(this.productForm.get('urls'));
+        return this.productForm.get('urls') as FormArray;
+    }
+
     onCategoryChange() {
         this.scrapingService.scrapingData.productPostDTO.subCategoryId =
-            this.Category.id;
+            this.productForm.get('Category').value.id;
     }
+
     onSubCategoryChange() {
         this.scrapingService.scrapingData.productPostDTO.subCategoryId =
-            this.SubCategory.id;
+            this.productForm.get('SubCategory').value.id;
     }
+
     onBrandChange() {
         this.scrapingService.scrapingData.productPostDTO.brandId =
-            this.Brand.id;
+            this.productForm.get('Brand').value.id;
     }
 
     addUrl(): void {
-        this.scrapingService.addUrl();
-    }
-
-    deleteUrl(index: number): void {
-        this.scrapingService.deleteUrl(index);
-    }
-    show(Severity: string, Message: string = 'Please wait...') {
-        this.messageService.add({
-            severity: Severity,
-            summary: Severity,
-            detail: Message,
-        });
-    }
-    saveProduct() {}
-    // Navigation
-    next() {
-        this.isScraping = true;
-        console.log(this.scrapingService.scrapingData.productPostDTO);
-        this.scrapingService.GetData(this.scrapingService.urls).subscribe(
-            // Success
-            (data) => {
-                this.isScraping = false;
-                if (data.length > 0) {
-                    this.scrapingService.scrapingData.productDetailDTO = data;
-                    this.isScraping = true;
-                    this.router.navigate([
-                        '/admin/products/add-product/review',
-                    ]);
-                } else {
-                    console.error('No data found for the given URLs');
-                }
-            },
-            // Error
-            (error) => {
-                this.isScraping = false;
-                console.error('Error', error);
-            }
+        this.urls.push(
+            this.fb.group({
+                url: ['', [Validators.required, validUrlValidator()]],
+            })
         );
     }
 
-    back() {
-        this.router.navigate(['/admin/products']);
+    deleteUrl(index: number): void {
+        this.urls.removeAt(index);
+    }
+
+    show(severity: string, message: string = 'Please wait...') {
+        this.messageService.add({
+            severity: severity,
+            summary: severity,
+            detail: message,
+        });
+    }
+
+    isValidUrl(url: any): boolean {
+        if (typeof url !== 'string') {
+            return false;
+        }
+        const validDomains = [
+            'amazon.eg',
+            'noon',
+            'aliexpress',
+            'jarir',
+            'extra',
+            'amazon.sa',
+        ];
+        return validDomains.some((domain) => url.includes(domain));
     }
 
     isNextButtonEnabled(): boolean {
         return (
-            !this.scrapingService.urls.some(
-                (url) => url === 'https://www.example.com/'
-            ) &&
-            this.scrapingService.urls.length > 0 &&
-            this.Brand != null &&
-            this.Category != null &&
-            this.SubCategory != null &&
-            this.scrapingService.scrapingData.productPostDTO.name_Global !=
-                '' &&
-            this.scrapingService.scrapingData.productPostDTO
-                .description_Global != ''
-                
+            this.productForm.valid &&
+            this.urls.controls.every((urlControl) =>
+                this.isValidUrl(urlControl.value.url)
+            )
         );
     }
-    isValidUlr(url: string): boolean {
-        // Only Accept Urls within Domain Amazon.eg,noon,aliexpress,jarir,extra,amazon.sa
-        return (
-            url.includes('amazon.eg') ||
-            url.includes('noon') ||
-            url.includes('aliexpress') ||
-            url.includes('jarir') ||
-            url.includes('extra') ||
-            url.includes('amazon.sa')
-        );
+
+    onSubmit() {
+        if (this.productForm.invalid) {
+            this.productForm.markAllAsTouched();
+            this.messageService.add({
+                severity: 'error',
+                summary: 'Error',
+                detail: 'Please fill all the required fields properly.',
+            });
+            return;
+        }
+        this.scrapingService.scrapingData.productPostDTO = {
+            name_Local: this.productForm.value.name_Local,
+            name_Global: this.productForm.value.name_Global,
+            description_Local: this.productForm.value.description_Local,
+            description_Global: this.productForm.value.description_Global,
+            subCategoryId: this.productForm.value.SubCategory.id,
+            brandId: this.productForm.value.Brand.id,
+        };
+        this.isScraping = true;
+        console.log(this.scrapingService.scrapingData.productPostDTO);
+
+        this.scrapingService
+            .GetData(
+                this.productForm.value.urls.map((urlGroup) => urlGroup.url)
+            )
+            .subscribe(
+                (data) => {
+                    this.isScraping = false;
+                    if (data.length > 0) {
+                        this.scrapingService.scrapingData.productDetailDTO =
+                            data;
+                        this.scrapingService.urls =
+                            data.map(
+                                (x) => x.productlink1
+                            );
+                        console.log(
+                            this.scrapingService.scrapingData.productDetailDTO
+                        );
+                        console.log(this.scrapingService.urls);
+                        this.router.navigate([
+                            '/admin/products/add-product/review',
+                        ]);
+                    } else {
+                        this.messageService.add({
+                            severity: 'error',
+                            summary: 'Error',
+                            detail: 'No data found for any of the given URLs',
+                        });
+                        console.error('No data found for the given URLs');
+                    }
+                },
+                (error) => {
+                    this.isScraping = false;
+                    console.error('Error', error);
+                }
+            );
+    }
+
+    back() {
+        this.router.navigate(['/admin/products']);
     }
 }
