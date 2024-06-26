1eba5c41444b103be2ffa57955c7912b058e429b
ahmedl3swy <ahmed.a.alesawy@gmail.com>
2024-06-26 13:02:27 +0300

    Switched main route to admin as Angular Deployment doesn	 allow url navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'admin', pathMatch: 'full' },
   // { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
