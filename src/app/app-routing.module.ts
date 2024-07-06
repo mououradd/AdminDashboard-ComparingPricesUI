b945218cbe8bd3385d1755f251c3506996775120
abdullah-mohamed9 <121031158+abdullah-mohamed9@users.noreply.github.com>
2024-07-06 20:48:19 +0300

    privacy policy component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,26 +4,28 @@ import { notfoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { PrivacyPolicyComponent } from './userLayout/components/privacy-policy/privacy-policy.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'admin', pathMatch: 'full' },
-  // { path: 'home', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  {
-    path: 'admin',
-    loadChildren: () => import('./modules/admin/admin-routing.module').then(m => m.AdminRoutingModule)
-  },
-  {
-    path: '',
-    loadChildren: () => import('./modules/user/user-routing.module').then(m => m.UserRoutingModule)
-  },
-  { path: 'notfound', component: notfoundComponent },
-  { path: '**', redirectTo: '/notfound' }
+    { path: '', redirectTo: 'admin', pathMatch: 'full' },
+    // { path: 'home', component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
+    {path: 'privacy-policy', component : PrivacyPolicyComponent , pathMatch: 'full'},
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'admin',
+        loadChildren: () => import('./modules/admin/admin-routing.module').then(m => m.AdminRoutingModule)
+    },
+    {
+        path: '',
+        loadChildren: () => import('./modules/user/user-routing.module').then(m => m.UserRoutingModule)
+    },
+    { path: 'notfound', component: notfoundComponent },
+    { path: '**', redirectTo: '/notfound' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
