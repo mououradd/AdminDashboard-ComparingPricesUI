4325485e4fff9ae25cdd91d696f49aa87e421ba9
abdullah-mohamed9 <121031158+abdullah-mohamed9@users.noreply.github.com>
2024-07-08 08:46:16 +0300

    save nothing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -23,6 +23,7 @@ export class HomeComponent {
             this.images = [];
             this.images.push('https://robcubbon.com/wp-content/uploads/2016/01/Why-Product-Comparison-Blog-Posts-Are-Massive-Earners-black-board-comparing-two-products.jpg');
             this.images.push('https://static.digit.in/default/14649samsungs23-ultra1-2-20231280x720-7e165d4c68.jpeg');
+            this.images.push('https://media.licdn.com/dms/image/C4E12AQGLInZwLBXTLw/article-cover_image-shrink_720_1280/0/1602734977905?e=2147483647&v=beta&t=O0uxWk1mCQBKhT4kTGH5R4AKgDl1HFwf4xJSQoV0u9M');
         }
 
 }
